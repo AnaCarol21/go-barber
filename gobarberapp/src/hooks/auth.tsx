7802c1534b2891ffac2c5b9a1c916d6c46a3bf28
AnaCarol21 <carol.agl@live.com>
2020-05-19 17:22:30 -0300

    :rocket: Add private routes

--- gobarberapp/src/hooks/auth.tsx
+++ gobarberapp/src/hooks/auth.tsx
@@ -21,6 +21,7 @@ interface SingICredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SingICredentials): Promise<void>;
   signOut(): void;
 }
@@ -29,6 +30,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageStoragedData(): Promise<void> {
@@ -39,6 +41,7 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
     loadStorageStoragedData();
   }, []);
@@ -66,7 +69,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
