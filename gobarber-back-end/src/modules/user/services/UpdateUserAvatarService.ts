b505fc6be8cf8689aa69de9cdaf5743e96605fec
AnaCarol21 <carol.agl@live.com>
2020-05-21 18:21:05 -0300

    :rocket: Add DDD concept

---
+++ gobarber-back-end/src/modules/user/services/UpdateUserAvatarService.ts
@@ -0,0 +1,36 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import AppError from '../errors/AppError';
+import User from '../models/Users';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+    const user = await usersRepository.findOne(user_id);
+    if (!user) {
+      throw new AppError('Only authenticated users can change avatar', 401);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+export default UpdateUserAvatarService;
