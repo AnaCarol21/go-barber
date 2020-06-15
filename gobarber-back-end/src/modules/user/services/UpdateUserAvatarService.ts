05f6b5612381a92a0f573e1ae9184be968569897
AnaCarol21 <carol.agl@live.com>
2020-06-15 20:11:47 -0300

    :wrench: Configuring imports

--- gobarber-back-end/src/modules/user/services/UpdateUserAvatarService.ts
+++ gobarber-back-end/src/modules/user/services/UpdateUserAvatarService.ts
@@ -2,9 +2,9 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
-import uploadConfig from '../config/upload';
-import AppError from '../errors/AppError';
-import User from '../models/Users';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import User from '@modules/user/infra/typeorm/entities/Users';
 
 interface Request {
   user_id: string;
--- iniciando-back-end/src/config/auth.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  jwt: {
-    secret: '7367a611940bc9b945e1e2057e63d8be',
-    expiresIn: '1d',
-  },
-};
