0b08ffcba491358e681f6f5ae4c319c18825ecdb
AnaCarol21 <carol.agl@live.com>
2020-07-15 17:43:39 -0300

    :vertical_traffic_light: Add data validation

--- gobarber-back-end/src/modules/users/infra/http/routes/users.routes.ts
+++ gobarber-back-end/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
@@ -12,7 +14,17 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
