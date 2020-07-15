5a4405d7958fab27a4520cf954646cb1542c2441
AnaCarol21 <carol.agl@live.com>
2020-07-15 16:18:42 -0300

    :open_file_folder: Add notifications structure

---
+++ gobarber-back-end/src/modules/notifications/infra/typeorm/schemas/Notification.ts
@@ -0,0 +1,28 @@
+import {
+  ObjectID,
+  Entity,
+  UpdateDateColumn,
+  Column,
+  CreateDateColumn,
+  ObjectIdColumn } from 'typeorm';
+
+@Entity('notifications')
+export default class Notification {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  content: string;
+
+  @Column('uuid')
+  recipient_id: string;
+
+  @Column({default: false})
+  read: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
