cfdfecf4a50e20d0e2cf172b93c64053163157b2
AnaCarol21 <carol.agl@live.com>
2020-07-17 10:05:15 -0300

    :school_satchel: Add cache invalidation

--- gobarber-back-end/src/modules/users/services/CreateUserService.ts
+++ gobarber-back-end/src/modules/users/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/Users';
 import { inject, injectable } from 'tsyringe';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
@@ -18,6 +19,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -34,6 +38,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
