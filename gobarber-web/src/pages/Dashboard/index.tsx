119037db2f437dba28c798acef0c03fde16cf141
AnaCarol21 <carol.agl@live.com>
2020-07-21 15:38:28 -0300

    :art: Add next appointment

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
@@ -24,6 +33,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/62665967?s=460&u=0286bf1a831f762c4268b9babe6d1955a0ce70b9&v=4"
+                alt="Ana Lima"
+              />
+              <strong>Ana Lima</strong>
+              <span>
+                <FiClock /> 8:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Calendar />
+        </Schedule>
+      </Content>
     </Container>
   );
 };
