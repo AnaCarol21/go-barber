06be3946139d1d277344672b6c0d64f19bd2fc64
AnaCarol21 <carol.agl@live.com>
2020-07-22 09:09:05 -0300

    :rocket: Add month availability

--- gobarber-web/src/hooks/auth.tsx
+++ gobarber-web/src/hooks/auth.tsx
@@ -31,16 +31,21 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
       return { token, user: JSON.parse(user) };
     }
+
     return {} as AuthState;
   });
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', { email, password });
 
     const { token, user } = response.data;
+
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
