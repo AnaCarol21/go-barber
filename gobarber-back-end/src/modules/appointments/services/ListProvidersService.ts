d2c50a9257f314823bfdb71d95866c70f3673b4c
AnaCarol21 <carol.agl@live.com>
2020-07-26 13:01:03 -0300

    :hammer: Add classToClass on save

--- gobarber-back-end/src/modules/appointments/services/ListProvidersService.ts
+++ gobarber-back-end/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import User from '@modules/users/infra/typeorm/entities/Users';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 
 interface IRequest {
   user_id: string;
@@ -27,7 +28,10 @@ export default class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users),
+      );
     }
 
     return users;
