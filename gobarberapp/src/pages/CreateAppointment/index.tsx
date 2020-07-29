10e5ff3ca800cb45b312237f1fec0527462125e8
AnaCarol21 <carol.agl@live.com>
2020-07-29 14:40:25 -0300

    :rocket: Add request day availability from api

--- gobarberapp/src/pages/CreateAppointment/index.tsx
+++ gobarberapp/src/pages/CreateAppointment/index.tsx
@@ -35,6 +35,11 @@ export interface Provider {
   avatar_url: string;
 }
 
+interface AvailabilityItem {
+  hour: number;
+  available: boolean;
+}
+
 const CreateAppointment: React.FC = () => {
   const { user } = useAuth();
   const route = useRoute();
@@ -42,6 +47,7 @@ const CreateAppointment: React.FC = () => {
 
   const routeParams = route.params as RouteParams;
 
+  const [availability, setAvailability] = useState<AvailabilityItem[]>([]);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
 
@@ -55,6 +61,20 @@ const CreateAppointment: React.FC = () => {
       setProviders(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    api
+      .get(`providers/${selectedProvider}/day-availability`, {
+        params: {
+          year: selectedDate.getFullYear(),
+          month: selectedDate.getMonth() + 1,
+          day: selectedDate.getDate(),
+        },
+      })
+      .then((response) => {
+        setAvailability(response.data);
+      });
+  }, [selectedDate, selectedProvider]);
 
   const navigateBack = useCallback(() => {
     goBack();
@@ -116,13 +136,16 @@ const CreateAppointment: React.FC = () => {
         <OpenDatePickerButtonText>
           Selecionar outra data
         </OpenDatePickerButtonText>
-        <DateTimerPicker
-          mode="date"
-          display="calendar"
-          onChange={handleDateChanged}
-          textColor="#f4ede8"
-          value={selectedDate}
-        />
+
+        {showDatePicker && (
+          <DateTimerPicker
+            mode="date"
+            display="calendar"
+            onChange={handleDateChanged}
+            textColor="#f4ede8"
+            value={selectedDate}
+          />
+        )}
       </Calendar>
     </Container>
   );
